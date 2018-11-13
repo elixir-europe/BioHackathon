0a05e4e7caabe5550b5eacffeb26f527376a0d1a
Luca Cherubin <luca.cherubin@gmail.com>
2018-11-13 16:39:49 +0100

    Starts the MarRef api proof of concept

---
+++ interoperability/Data clearinghouse, validation and curation of BioSamples_ENA_Breeding API endpoints_MAR databases/marref_biosamples/src/main/java/org/elixir/marref/MarrefApplication.java
@@ -0,0 +1,12 @@
+package org.elixir.marref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarrefApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarrefApplication.class, args);
+    }
+}
---
+++ interoperability/Data clearinghouse, validation and curation of BioSamples_ENA_Breeding API endpoints_MAR databases/marref_biosamples/src/test/java/org/elixir/marref/MarrefApplicationTests.java
@@ -0,0 +1,16 @@
+package org.elixir.marref;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MarrefApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
