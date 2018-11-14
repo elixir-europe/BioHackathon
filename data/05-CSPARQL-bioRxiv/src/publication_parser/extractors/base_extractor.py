17527f962420bbbdc8ea937fba40636f86c1365a
Valentin Grouès <valentin.groues@uni.lu>
2018-11-14 15:07:15 +0100

    remove unused fields for biorxiv parser

--- data/05-CSPARQL-bioRxiv/src/publication_parser/extractors/base_extractor.py
+++ data/05-CSPARQL-bioRxiv/src/publication_parser/extractors/base_extractor.py
@@ -1,5 +1,3 @@
-import os
-import random
 from abc import ABC, abstractmethod
 
 from typing import Optional, Dict
@@ -14,14 +12,14 @@ class BaseExtractor(ABC):
         returns a dict otherwise
         """
 
-
-class DummyExtractor(BaseExtractor):
-    def parse(self, filename: str) -> Optional[Dict[str, str]]:
-        return {
-            'title': os.path.basename(filename),
-            'doi': random.randint(0, 9999),
-            'author': 'Mr. Cool',
-            'year': random.randint(1600, 2019),
-            'abstract': 'blabla',
-            'url': 'example.org'
-        }
+#
+# class DummyExtractor(BaseExtractor):
+#     def parse(self, filename: str) -> Optional[Dict[str, str]]:
+#         return {
+#             'title': os.path.basename(filename),
+#             'doi': random.randint(0, 9999),
+#             'author': 'Mr. Cool',
+#             'year': random.randint(1600, 2019),
+#             'abstract': 'blabla',
+#             'url': 'example.org'
+#         }
