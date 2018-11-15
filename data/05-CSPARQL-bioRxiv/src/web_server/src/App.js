4eba292f29f24241fc047de286a69d761044283a
6br <tfdvrt@gmail.com>
2018-11-15 21:07:40 +0900

    Add parcel

--- data/05-CSPARQL-bioRxiv/src/web_server/src/App.js
+++ data/05-CSPARQL-bioRxiv/src/web_server/src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
-import './App.css';
 import Builder from './Builder';
+import * as QueryString from 'query-string';
 
+const search_items = ["hasTitle", "hasAuthors", "hasYear", "hasUrl", "hasDOI", "hasAbstract"];
 class App extends Component {
   constructor() {
     super();
+    const parsed = QueryString.parse(location.search);
+    let query = parsed["q"] !== undefined ? parsed["q"] : "";
+    let queryList = query.split(" ").map(item => {
+    let items = item.split(":");
+    if (search_items.indexOf(items[0]) < 0) {
+        items[0] = search_items[0];
+    }
+    if (items[1] === undefined) {
+        items[1] = "";
+    }
+    return items;
+    });
+    if (queryList.length === 0) {
+      queryList = [[search_items[0], ""]]
+    }
     this.state = {
-      query: "{{ query if query is not none }}",
-      advancedQuery: "",
-      advance: true,
-      queryList: [["Title", ""]],
+        query,
+        advance: true,
+        queryList
     };
     this.addQueryList = this.addQueryList.bind(this);
     this.updateValue = this.updateValue.bind(this);
@@ -19,33 +34,35 @@ class App extends Component {
   }
 
   updateQueryList(index, type, content) {
-    let queryList = this.state.queryList
+    let queryList = this.state.queryList;
     if (type === "type") {
-      queryList[index][0] = content
+      queryList[index][0] = content;
     } else {
-      queryList[index][1] = content
+      queryList[index][1] = content;
     }
-    this.setState({queryList})
-    this.updateAdvancedQuery();
+    this.setState({queryList});
+    this.updateAdvancedQuery(queryList);
   }
 
   removeQueryList(index) {
-    this.setState({queryList: this.state.queryList.filter((n, i) => i !== index)});
-    this.updateAdvancedQuery();
+    let queryList = this.state.queryList.filter((n, i) => i !== index);
+    this.setState({queryList});
+    this.updateAdvancedQuery(queryList);
   }
 
   addQueryList() {
-    this.setState({queryList: this.state.queryList.concat([["Title", ""]]) });
-    this.updateAdvancedQuery();
+    let queryList = this.state.queryList.concat([[search_items[0], ""]]);
+    this.setState({queryList });
+    this.updateAdvancedQuery(queryList);
   }
 
   updateValue(value) {
     this.setState({query: value});
   }
 
-  updateAdvancedQuery() {
-    let advancedQuery = this.state.queryList.filter(item => item[1] !== "").map(query => `${query[0]}:${query[1]}`).join(" ") ;
-    this.setState({advancedQuery});
+  updateAdvancedQuery(queryList) {
+    let advancedQuery = queryList.filter(item => item[1] !== "").map(query => `${query[0]}:${query[1]}`).join(" ");
+    this.setState({query: advancedQuery});
   }
 
   render() {
@@ -53,19 +70,17 @@ class App extends Component {
       <div className="App">
         <form className="form-group row" action="/query">
           <div className="input-group mb-3">
-            <div className="input-group-prepend">
-              <button className="btn btn-outline-secondary" type="button" id="button-addon1" onClick={() => this.setState({advance: !this.state.advance})}>Advanced</button>
-            </div>
-            <input type="text" className="form-control" name="q" placeholder="" aria-label="search field" disabled={this.state.advance} aria-describedby="button-addon1" value={this.state.advance ?  this.state.advancedQuery :this.state.query} onChange={event => this.updateValue(event.target.value)} />
+            <input type="text" className="form-control" name="q" placeholder="use keywords such as: hasDoi:10.1000/182" aria-label="search field"  aria-describedby="button-addon1" value={this.state.query} onChange={event => this.updateValue(event.target.value)} />
             <div className="input-group-append">
               <button className="btn btn-outline-secondary" type="submit" id="button-addon1"><i className="fa fa-search"></i></button>
             </div>
           </div>
-          { this.state.advance && <Builder queryList={this.state.queryList} addQueryList={this.addQueryList} removeQueryList={this.removeQueryList} updateQueryList={this.updateQueryList} /> }
+          { this.state.advance && <Builder queryList={this.state.queryList} addQueryList={this.addQueryList} removeQueryList={this.removeQueryList} updateQueryList={this.updateQueryList} search_items={search_items} /> }
         </form>
       </div>
     );
   }
 }
+
 
 export default App;
---
+++ data/05-CSPARQL-bioRxiv/src/web_server/src/Builder.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Builder extends Component {
+    render() {
+        return (
+          <div className="input-group mb-3 row">
+            <div className="mx-auto my-auto">
+              <h5>Query Interface</h5>
+              {this.props.queryList.map((query, index) => {
+                return (
+                    <div className="d-flex" key={index}>
+                        <select className="custom-select form-control" id="inputGroupSelect01" value={query[0]} onChange={event => this.props.updateQueryList(index, "type", event.target.value)}>
+                            {this.props.search_items.map((search_item, search_item_index) => 
+                                <option key={search_item_index} value={search_item}>{search_item}</option>
+                            )}
+                        </select>
+                        <input type="text" className="form-control search-query" value={query[1]} onChange={ event => this.props.updateQueryList(index, "text", event.target.value)} />
+                        { index !== 0 && <button className="btn btn-outline-secondary" type="button" id="button-addon1" onClick={() => this.props.removeQueryList(index)}><i className="fa fa-minus"></i></button>}
+                        { (index === this.props.queryList.length - 1) && <button className="btn btn-outline-secondary" type="button" id="button-addon1" onClick={() => this.props.addQueryList()}><i className="fa fa-plus"></i></button>}
+                    </div>
+                )
+              })}
+            </div>
+          </div>
+        );
+      }
+}
+
+export default Builder;
