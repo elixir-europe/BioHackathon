51f87258af7d4f193c5fd17b8c57f91585244fa9
6br <tfdvrt@gmail.com>
2018-11-15 21:10:50 +0900

    Remove unused libraries

--- data/05-CSPARQL-bioRxiv/src/web_server/run.py
+++ data/05-CSPARQL-bioRxiv/src/web_server/run.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask, render_template, request
 from flask_paginate import Pagination, get_page_parameter
 
-from web_server.sparql_wrapper import (
+from sparql_wrapper import (
     execute_query,
     get_total_papers,
     get_properties)
