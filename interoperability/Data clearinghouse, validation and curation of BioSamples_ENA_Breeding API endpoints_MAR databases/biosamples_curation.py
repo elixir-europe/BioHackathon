cb7a109a24a8d3f4b5d8cb1fffabe9953ae37858
Luca Cherubin <luca.cherubin@gmail.com>
2018-11-15 14:28:42 +0100

    Adds an example of the produced bioschemas for MarRef and some line to get a sample from the API

--- interoperability/Data clearinghouse, validation and curation of BioSamples_ENA_Breeding API endpoints_MAR databases/biosamples_curation.py
+++ interoperability/Data clearinghouse, validation and curation of BioSamples_ENA_Breeding API endpoints_MAR databases/biosamples_curation.py
@@ -1,3 +1,11 @@
+import requests
+import json
+from biosamples_v4.api import Client as biosd_client
+from biosamples_v4.aap import Client as aap_client
 
 if __name__ == "__main__":
-    print("Hello world")
+    acc = "MMP07276874"
+    sample_url = "http://localhost:7777/marref/api/samples" . acc
+    res = requests.get(sample_url)
+    if res.status_code == 200:
+        print(json.dumps(res.json(), indent=2))
