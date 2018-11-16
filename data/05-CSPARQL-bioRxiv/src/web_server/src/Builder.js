2905e2891368b281367a2ac7af5b07bf1465145e
6br <tfdvrt@gmail.com>
2018-11-16 18:24:06 +0900

    Fix components id

--- data/05-CSPARQL-bioRxiv/src/web_server/src/Builder.js
+++ data/05-CSPARQL-bioRxiv/src/web_server/src/Builder.js
@@ -15,8 +15,8 @@ class Builder extends Component {
                             )}
                         </select>
                         <input type="text" className="form-control search-query" value={query[1]} onChange={ event => this.props.updateQueryList(index, "text", event.target.value)} />
-                        { index !== 0 && <button className="btn btn-outline-secondary" type="button" id="button-addon1" onClick={() => this.props.removeQueryList(index)}><i className="fa fa-minus"></i></button>}
-                        { (index === this.props.queryList.length - 1) && <button className="btn btn-outline-secondary" type="button" id="button-addon1" onClick={() => this.props.addQueryList()}><i className="fa fa-plus"></i></button>}
+                        { index !== 0 && <button className="btn btn-outline-secondary" type="button" id="button" onClick={() => this.props.removeQueryList(index)}><i className="fa fa-minus"></i></button>}
+                        { (index === this.props.queryList.length - 1) && <button className="btn btn-outline-secondary" type="button" id="button" onClick={() => this.props.addQueryList()}><i className="fa fa-plus"></i></button>}
                     </div>
                 )
               })}
