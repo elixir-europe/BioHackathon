2a9549a06d642f5bad9c10da02db48cbb5009bda
6br <tfdvrt@gmail.com>
2018-11-16 18:39:35 +0900

    Refactor js scripts

--- data/05-CSPARQL-bioRxiv/src/web_server/src/App.js
+++ data/05-CSPARQL-bioRxiv/src/web_server/src/App.js
@@ -5,28 +5,29 @@ import * as QueryString from 'query-string';
 class App extends Component {
   constructor(props) {
     super();
-    let searchItems = props.searchItems
+    const searchItems = props.searchItems;
 
     const parsed = QueryString.parse(location.search);
-    let query = parsed["q"] !== undefined ? parsed["q"] : "";
+    const query = parsed["q"] !== undefined ? parsed["q"] : "";
     let queryList = query.split(" ").map(item => {
-    let items = item.split(":");
-        if (searchItems.indexOf(items[0]) < 0) {
-            items[0] = searchItems[0];
-        }
-        if (items[1] === undefined) {
-            items[1] = "";
-        }
-        return items;
+      let items = item.split(":");
+      if (searchItems.indexOf(items[0]) < 0) {
+          items[0] = searchItems[0];
+      }
+      if (items[1] === undefined) {
+          items[1] = "";
+      }
+      return items;
     });
     if (queryList.length === 0) {
-      queryList = [[searchItems[0], ""]]
+      queryList = [[searchItems[0], ""]]; // Initialize with a default item.
     }
+
     this.state = {
-        query,
-        advance: true,
-        queryList,
-        searchItems
+      query,
+      advance: true,
+      queryList,
+      searchItems
     };
     this.addQueryList = this.addQueryList.bind(this);
     this.updateValue = this.updateValue.bind(this);
@@ -47,14 +48,14 @@ class App extends Component {
   }
 
   removeQueryList(index) {
-    let queryList = this.state.queryList.filter((n, i) => i !== index);
+    const queryList = this.state.queryList.filter((n, i) => i !== index);
     this.setState({queryList});
     this.updateAdvancedQuery(queryList);
   }
 
   addQueryList() {
-    let queryList = this.state.queryList.concat([[this.props.searchItems[0], ""]]);
-    this.setState({queryList });
+    const queryList = this.state.queryList.concat([[this.props.searchItems[0], ""]]);
+    this.setState({queryList});
     this.updateAdvancedQuery(queryList);
   }
 
@@ -63,7 +64,7 @@ class App extends Component {
   }
 
   updateAdvancedQuery(queryList) {
-    let advancedQuery = queryList.filter(item => item[1] !== "").map(query => `${query[0]}:${query[1]}`).join(" ");
+    const advancedQuery = queryList.filter(item => item[1] !== "").map(query => `${query[0]}:${query[1]}`).join(" ");
     this.setState({query: advancedQuery});
   }
 
